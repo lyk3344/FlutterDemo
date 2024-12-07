889778cb4f5173325c9ed1b60f9d46bec2da686a
lyk3344 <303231723@qq.com>
2024-12-08 02:10:09 +0800

    增加边距，完善细节

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -24,27 +24,50 @@ class LoginPage extends StatelessWidget {
                 color: Colors.red,
                 //继续嵌套使用Column组件来布局账号密码输入框、登录按钮、注册按钮这三个组件
                 child: Column(
+                  //CrossAxisAlignment取stretch时，Column才会尽可能地占据父组件提供的最大宽度
+                  //并且要求其所有子组件的宽度也要与自身的宽度相一致
+                  //对于其它属性值，Column会首先要求子组件按照不超过父组件提供的最大宽度进行布局，
+                  //然后自身宽度则由宽度最大的子组件决定
                   crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Container(
-                          child: Text('邮箱'),
-                          color: Colors.brown,
+                    //Column布局的输入框文本组件是居中对齐的
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 24, right: 24, bottom: 12),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            child: Text('邮箱'),
+                            color: Colors.brown,
+                          ),
+                          Container(
+                            child: Text('密码'),
+                            color: Colors.brown,
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 24, right: 24, top: 12, bottom: 12),
+                      child: Container(
+                        child: Text('登录按钮'),
+                        color: Colors.brown,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(
+                          left: 24, right: 24, top: 12, bottom: 12),
+                      child: Container(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text('没有账号？'),
+                            Text('立即注册'),
+                          ],
                         ),
-                        Container(
-                          child: Text('密码'),
-                          color: Colors.brown,
-                        )
-                      ],
-                    ),
-                    Container(
-                      child: Text('登录按钮'),
-                      color: Colors.brown,
-                    ),
-                    Container(
-                      child: Text('注册提示'),
-                      color: Colors.brown,
+                        color: Colors.brown,
+                      ),
                     )
                   ],
                 ),
